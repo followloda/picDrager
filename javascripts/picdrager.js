70602ba86fc48e02d46d9f11743ba9e20ff0ef65
singlepig <gys.nanjing@gmail.com>
2017-05-11 02:09:32 +0800

    实现读取和显示自定义目录下图片
    - [x] 自定义读取目录
    - [x] 显示目录中图片
    - [x] 使图片可拖拽

--- javascripts/picdrager.js
+++ javascripts/picdrager.js
@@ -1,9 +1,10 @@
+// for debug
 var filtered_files = [];
 var files_out;
 
 function scandir(files) {
     files_out = files;
-    console.log("do scan");
+
     if (files.length == 0) {
         alert('该目录下没有文件');
         return false;
@@ -12,25 +13,50 @@ function scandir(files) {
     }
 
     for (i = 0; i < files.length; i++) {
-        if (files[i].type == "image/png") {
+        if (files[i].type == "image/png" || files[i].type == "image/jpeg") {
             filtered_files.push(files[i]);
+            addGridlyPic(files[i]);
         } else {
-            console.log(files[i].type);
+            console.log(files[i].name, " is invalid!");
         }
     }
+    rearrange();
+}
 
+function onloadCb(img) {
+    return function (evt) {
+        img.attr('src', evt.target.result);
+    }
+}
 
-    // var reader = new FileReader();
-    // reader.onload = function (e) {
-    //     var data = e.target.result;
-    //     //加载图片获取图片真实宽度和高度
-    //     var image = new Image();
-    //     image.onload = function () {
-    //         var width = image.width;
-    //         var height = image.height;
-    //         alert(width + '======' + height + "=====" + f.size);
-    //     };
-    //     image.src = data;
-    // };
-    // reader.readAsDataURL(f);
+function addGridlyPic(pic) {
+    //创建包含pic的brick div
+
+    //创建brick
+    $('<div />', {
+            class: 'brick small'
+        })
+        .appendTo($('.gridly'));
+
+    //创建img
+    var img = $('<img />', {
+        src: '',
+        width: '251px',
+        height: '334px'
+    });
+
+    //获取img的src
+    var reader = new FileReader();
+    reader.onload = onloadCb(img);
+    reader.readAsDataURL(pic);
+
+    img.appendTo($('.brick:last'));
+}
+
+function rearrange() {
+    $('.gridly').gridly({
+        // base: 6, // px
+        // gutter: 1, // px
+        columns: 16
+    });
 }
